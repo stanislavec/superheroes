d7913d3667b43243f66aaf433479efd712131bc8
Stanislaveec <blizzak007@yandex.ru>
2019-04-07 22:52:35 +0300

    Updates

---
+++ precache-manifest.c9b5709084b413304f97e894fddcfc98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "d0fd9693d061912fd5c3",
+    "url": "/my-app/static/js/main.4f26dbe3.chunk.js"
+  },
+  {
+    "revision": "10a9045124fa2fa5ad77",
+    "url": "/my-app/static/js/2.f32d8b25.chunk.js"
+  },
+  {
+    "revision": "d0fd9693d061912fd5c3",
+    "url": "/my-app/static/css/main.ee758fdf.chunk.css"
+  },
+  {
+    "revision": "af52f5ee620d2c73ecebc62a68a89975",
+    "url": "/my-app/index.html"
+  }
+];
